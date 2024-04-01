f73bb8bb32f87e2ac10483e5679240c7aceb674f
vasilije06 <cvecje@gmail.com>
2024-04-01 22:41:20 +0200

    Dodata funkcija pictureDraw i funkcija CollisionDetection

--- Papir Kamen Makaze/main.c
+++ Papir Kamen Makaze/main.c
@@ -1,6 +1,9 @@
 #include <raylib.h>
 #include <stdio.h>
 
+void pictureRender();
+void collisionDetection();
+
 int main()
 {
 
@@ -8,10 +11,39 @@ int main()
 
 	while (!WindowShouldClose())
 	{
-		BeginDrawing();
-		 ClearBackground(WHITE);
-		EndDrawing();
+		
+		pictureRender();
+		collisionDetection();
 
 	}
 	CloseWindow();
+}
+
+void pictureRender()
+{ 
+
+BeginDrawing();
+
+	ClearBackground(GRAY);
+	DrawRectangle(100, 450, 150, 50, WHITE);
+		DrawText("kamen", 110, 450, 20, BLACK);
+
+	DrawRectangle(300, 450, 150, 50, WHITE);
+		DrawText("papir", 310, 450, 20, BLACK);
+
+	DrawRectangle(500, 450, 150, 50, WHITE);
+		DrawText("makaze", 510, 450, 20, BLACK);
+
+	DrawText("Izaberite Kamen, Papir ili Makaze", 80, 200, 40, WHITE);
+
+EndDrawing();
+
+}
+
+void collisionDetection()
+{  
+	int mouseX = GetMouseX();
+	int mouseY = GetMouseY();
+	
+
 }
