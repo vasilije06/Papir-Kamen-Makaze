266c2793d507a1b6f7525718fb9bf06b18495be1
vasilije06 <cvecje@gmail.com>
2024-04-05 19:02:01 +0200

    testuranje dugmeta

--- Papir Kamen Makaze/main.c
+++ Papir Kamen Makaze/main.c
@@ -1,23 +1,54 @@
 #include <raylib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+int collisionDetection();
 void pictureRender();
-void collisionDetection();
+
 
 int main()
-{
+{	
+	SetTargetFPS(60);
 
 	InitWindow(800, 600, "test1");
 
 	while (!WindowShouldClose())
 	{
-		
-		collisionDetection();
 		pictureRender();
-		
-
+	
 	}
 	CloseWindow();
+}
+
+int collisionDetection(int pravac)
+{  
+	int rec1X = 100, recy = 450,
+		rec2x = 300, rec3x = 500;
+
+	int mouseX = GetMouseX();
+	int mouseY = GetMouseY();
+	
+	
+	if (1)
+	{
+
+		printf("mouseX: %d, mouseY: %d \n", mouseX, mouseY);
+
+	}
+
+	if (IsKeyPressed(KEY_B))
+	{
+
+		printf("stistuno \n");
+
+	}
+
+	if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && mouseX >= 100 && mouseY <150 + 150 && mouseY <= recy && mouseY >= recy + 50)
+	{
+		return pravac + 1;
+
+	}
+
 }
 
 void pictureRender()
@@ -25,29 +56,34 @@ void pictureRender()
 	int rec1X = 100, recy = 450,
 		rec2x = 300, rec3x = 500;
 
-BeginDrawing();
+	int smer = collisionDetection(0);
+	//char bojaPozadine[20] = "WHITE";
 
-	ClearBackground(GRAY);
-	DrawRectangle(rec1X, recy, 150, 50, WHITE);
-		DrawText("kamen", rec1X + 10, recy, 20, BLACK);
+	if (smer == 1)
+	{
 
-	DrawRectangle(rec2x, recy, 150, 50, WHITE);
-		DrawText("papir", rec2x + 10, recy, 20, BLACK);
+		printf("stuslo se");
 
-	DrawRectangle(rec3x, recy, 150, 50, WHITE);
-		DrawText("makaze", rec3x + 10, recy, 20, BLACK);
+	}
 
-	DrawText("Izaberite Kamen, Papir ili Makaze", 80, 200, 40, WHITE);
 
-EndDrawing();
+
+	BeginDrawing();
+
+
+		ClearBackground(GRAY);
+		DrawRectangle(rec1X, recy, 150, 50, WHITE);
+			DrawText("kamen", rec1X + 10, recy, 20, BLACK);
+
+		DrawRectangle(rec2x, recy, 150, 50, WHITE);
+			DrawText("papir", rec2x + 10, recy, 20, BLACK);
+
+		DrawRectangle(rec3x, recy, 150, 50, WHITE);
+			DrawText("makaze", rec3x + 10, recy, 20, BLACK);
+
+		DrawText("Izaberite Kamen, Papir ili Makaze", 80, 200, 40, WHITE);
+
+	EndDrawing();
 
 }
 
-void collisionDetection()
-{  
-	int mouseX = GetMouseX();
-	int mouseY = GetMouseY();
-	
-	if ()
-
-}
